dd5203af29f3325539f4f197df318d2ec1b1951e
Benedict <Benedictnnaoma0@gmail.com>
2023-11-27 00:17:26 +0100

    config cors con.

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const authRouter = require("./Routes/auth");
 const storedFoodRouter = require("./Routes/storedFood");
 const connectDB = require("./db/connection");
 const authMiddleware = require("./middleware/authMiddleware");
 require("dotenv").config();
 const Port = process.env.PORT || 5000;
-app.use(express.json());
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1", authMiddleware, storedFoodRouter);
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-// security packages
-const helmet = require("helmet");
-const cors = require("cors");
-const xss = require("xss-clean");
 
+app.use(express.json());
+
+// Set up CORS with specific allowed origins
 const corsOptions = {
-  origin: "http://localhost:5173", // Change this to your actual frontend origin
+  origin: ["http://localhost:5173"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1", authMiddleware, storedFoodRouter);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// security packages
+const helmet = require("helmet");
+const xss = require("xss-clean");
 
 app.use(helmet());
 app.use(xss());
